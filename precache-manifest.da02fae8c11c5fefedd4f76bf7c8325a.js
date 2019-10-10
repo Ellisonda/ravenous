f150d90617d48eae70b958f5076f92dba95fd8c4
Avier <avier.3@gmail.com>
2019-10-10 20:22:09 +0200

    Updates

---
+++ precache-manifest.da02fae8c11c5fefedd4f76bf7c8325a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "841285a49ae67a7be20367416b114975",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7bd3c3d8e951816b455",
+    "url": "/static/css/main.9bc4a970.chunk.css"
+  },
+  {
+    "revision": "f2f281fdafc7f46d9618",
+    "url": "/static/js/2.614c2985.chunk.js"
+  },
+  {
+    "revision": "a7bd3c3d8e951816b455",
+    "url": "/static/js/main.aaccaec2.chunk.js"
+  },
+  {
+    "revision": "d413c04b68339b296fcd",
+    "url": "/static/js/runtime-main.730f41a7.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
